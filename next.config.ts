a7995e181c2adb76c2e86340ca3185a4afb0f1ce
srishti Raghava <srishtiraghava1@gmail.com>
2026-07-30 14:20:16 +0530

    deployment issue

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // Tells Next.js to restrict file tracing strictly to this project directory
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
